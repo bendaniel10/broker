de4f95982e7ecd73bf2b85af76db21a42b6f5ff4
Daniel Ben <bendaniel20@gmail.com>
2020-02-26 19:46:55 +0100

    removed redundant class body.

--- broker-rewrite/src/main/kotlin/com/bendaniel10/broker/rewrite/response/BrokerResponseRewriter.kt
+++ broker-rewrite/src/main/kotlin/com/bendaniel10/broker/rewrite/response/BrokerResponseRewriter.kt
@@ -2,5 +2,4 @@ package com.bendaniel10.broker.rewrite.response
 
 import com.bendaniel10.broker.rewrite.BrokerRewriter
 
-interface BrokerResponseRewriter : BrokerRewriter {
-}
+interface BrokerResponseRewriter : BrokerRewriter
